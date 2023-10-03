c5bca54358524b075c08f07317dfdf9fac7ac64b
eogj1 <123607061+eogj1@users.noreply.github.com>
2023-10-02 20:41:57 -0400

    Moved hidePoweredBy

--- myApp.js
+++ myApp.js
@@ -2,12 +2,13 @@ const express = require('express');
 const helmet = require('helmet');
 const app = express();
 
+app.use(helmet.hidePoweredBy());
 
 module.exports = app;
 const api = require('./server.js');
 app.disable('strict-transport-security');
 app.use(express.static('public'));
-app.use(helmet.hidePoweredBy());
+
 app.use('/_api', api);
 app.get("/", function (request, response) {
   response.sendFile(__dirname + '/views/index.html');
