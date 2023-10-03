77d129588d973d8b0e70d75bbb656ab6cec10023
eogj1 <123607061+eogj1@users.noreply.github.com>
2023-10-02 23:01:30 -0400

    removed more statements that are included in app.use(helmet())

--- myApp.js
+++ myApp.js
@@ -10,41 +10,13 @@ app.use(helmet({
       defaultSrc: ["'self'"],
       scriptSrc: ["'self'", 'trusted-cdn.com'], 
     },
-  noCache: true // removes ability to cache (increase security, decrease performance)
-  }
+  noCache: true //  Disables caching for increased security (may decrease performance).
+  },
+  hsts: {
+    maxAge: timeInSeconds, force: true 
+  },
+  dnsPrefetchControl: disable
 }));
-
-// hidePoweredBy hides express as it is vulnerable
-app.use(
-  helmet.hidePoweredBy()
-);
-// frameguard secures against framing to prevent clickjacking
-app.use(
-  helmet.frameguard({
-    action: 'deny'
-  })
-);
-// xssFilter helps protect against XSS attacks
-app.use(
-  helmet.xssFilter()
-);
-// noSniff instructs the browser to not bypass the provided Content-type
-app.use(
-  helmet.noSniff()
-);
-// ieNoOpen prevents IE users from executing downloads in the site's trusted content
-app.use(
-  helmet.ieNoOpen()
-);
-// hsts() sets to use HTTPS for the next 90 days
-app.use(
-  helmet.hsts(
-   {maxAge: timeInSeconds, force: true})
-);
-// dnsPrefetchControl() helps control DNS prefetching (increase privacy at cost of performance)
-app.use(
-  helmet.dnsPrefetchControl()
-);
 
 module.exports = app;
 const api = require('./server.js');
